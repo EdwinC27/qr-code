58e326d4e2dce65b2c4c54ab96f38e18063d99d6
EdwinC27 <edwin.a.castillo.27@gmail.com>
2023-11-04 13:29:04 -0600

    first commit

---
+++ src/main/java/com/api/codigoqr/CodigoQrApplication.java
@@ -0,0 +1,13 @@
+package com.api.codigoqr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodigoQrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodigoQrApplication.class, args);
+	}
+
+}
