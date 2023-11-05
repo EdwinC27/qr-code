cd2b1a326570a18ccf77e73a2cf607ddbc96d7ba
EdwinC27 <edwin.a.castillo.27@gmail.com>
2023-11-05 12:59:43 -0600

    Delete import that we do not need

--- src/main/java/com/api/codigoqr/services/QrTextService.java
+++ src/main/java/com/api/codigoqr/services/QrTextService.java
@@ -1,19 +1,13 @@
 package com.api.codigoqr.services;
 
 import com.google.zxing.BarcodeFormat;
-import com.google.zxing.EncodeHintType;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
-import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import org.springframework.stereotype.Service;
 
 import java.awt.image.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Service
 public class QrTextService {
